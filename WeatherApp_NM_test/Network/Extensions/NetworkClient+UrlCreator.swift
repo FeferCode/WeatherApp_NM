787e40b67ab23b67475fd1f660a6a18335ed06bb
Jakub Majewski <jakub.majewski@outlook.com>
2017-11-26 02:05:29 +0100

    I think all is finished, there is no unit tests for now

--- WeatherApp_NM_test/Network/Extensions/NetworkClient+UrlCreator.swift
+++ WeatherApp_NM_test/Network/Extensions/NetworkClient+UrlCreator.swift
@@ -11,15 +11,19 @@ import Foundation
 extension NetworkClient {
     
     public func createUrl(byCityName: String) -> URL{
+        let cityName = byCityName.replacingOccurrences(of: " ", with: "%20")
         let file = Bundle.main.path(forResource: "ServerCalls", ofType: "plist")!
         let dictionary = NSDictionary(contentsOfFile: file)
         
         let baseURLString = dictionary!["api_base_call"] as! String
         let searchByCity = dictionary!["search_city_by_name"] as! String
         let units = dictionary!["metric_units"] as! String
+        let lang = dictionary!["lang"] as! String
         let apiKey = dictionary!["api_key"] as! String
         
-        let urlString = baseURLString + searchByCity + byCityName + units + apiKey
+        let userLang = NSLocalizedString("en", comment: "Search in user language")
+        let urlString = baseURLString + searchByCity + cityName + units + lang + userLang + apiKey
+        
         return URL(string: urlString)!
     }
     
